5391c15ae8f183ced719ba92999a51a02ecd72bd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-07-17 06:46:55 +0200

    fire: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: fire Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_fire.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_fire-user \
+    omni_fire-userdebug \
+    omni_fire-eng
